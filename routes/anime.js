25a5a0fbbb01fccd305cad941e013b8fa2c5a035
Roshan Praveen Shetty <47758717+RoshanPShetty@users.noreply.github.com>
2021-04-05 13:56:26 -0400

    updated get all

--- routes/anime.js
+++ routes/anime.js
@@ -28,67 +28,63 @@ router.post("/", async (req, res) => {
 });
 
 // GET: GETTING ALL THE ANIME
-router.get("/", async (req, res) => {
-  try {
-    const anime = await Anime.find();
-    res.json(anime);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get("/", (req, res) => {
+  Anime.find().then((anime) => res.send(anime)).catch((err) => {
+    res.status(500).send({ message: "Something went wrong" });
+  });
 
-// GET: GETTING ONE ANIME BASED ON ITS ID
-router.get("/:id", getAnime, (req, res) => {
-  res.send(res.anime);
-});
+  // GET: GETTING ONE ANIME BASED ON ITS ID
+  router.get("/:id", getAnime, (req, res) => {
+    res.send(res.anime);
+  });
 
-// PUT: UPDATING AN ANIME BASED ON ITS ID
-router.put("/:id", async (req, res) => {
-  const updateAnime = await Anime.findByIdAndUpdate(
-    req.params.id,
-    {
-      sources: req.body.sources,
-      title: req.body.title,
-      type: req.body.type,
-      episodes: req.body.episodes,
-      status: req.body.status,
-      year: req.body.year,
-      synonyms: req.body.synonyms,
-      tags: req.body.tags,
-      rating: req.body.rating,
-    },
-    { new: true }
-  );
+  // PUT: UPDATING AN ANIME BASED ON ITS ID
+  router.put("/:id", async (req, res) => {
+    const updateAnime = await Anime.findByIdAndUpdate(
+      req.params.id,
+      {
+        sources: req.body.sources,
+        title: req.body.title,
+        type: req.body.type,
+        episodes: req.body.episodes,
+        status: req.body.status,
+        year: req.body.year,
+        synonyms: req.body.synonyms,
+        tags: req.body.tags,
+        rating: req.body.rating,
+      },
+      { new: true }
+    );
 
-  if (!updateAnime) {
-    res.status(404).json({ message: "Anime not found" });
-  }
-  res.send(updateAnime);
-});
-
-// DELETE: DELETING AN ANIME BASED ON ITS ID
-router.delete("/:id", getAnime, async (req, res) => {
-  try {
-    await res.anime.remove();
-    res.json({ message: "Anime Deleted" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// MIDDLEWARE
-async function getAnime(req, res, next) {
-  try {
-    anime = await Anime.findById(req.params.id);
-    if (anime == null) {
-      return res.status(404).json({ message: "Anime not found" });
+    if (!updateAnime) {
+      res.status(404).json({ message: "Anime not found" });
     }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+    res.send(updateAnime);
+  });
+
+  // DELETE: DELETING AN ANIME BASED ON ITS ID
+  router.delete("/:id", getAnime, async (req, res) => {
+    try {
+      await res.anime.remove();
+      res.json({ message: "Anime Deleted" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+  // MIDDLEWARE
+  async function getAnime(req, res, next) {
+    try {
+      anime = await Anime.findById(req.params.id);
+      if (anime == null) {
+        return res.status(404).json({ message: "Anime not found" });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.anime = anime;
+    next();
   }
 
-  res.anime = anime;
-  next();
-}
-
-module.exports = router;
+  module.exports = router;
